59da24ee4aefa4cc5b2a7ff1a94c9028394bc908
penne <penne0505pp@gmail.com>
2025-05-08 22:50:23 +0900

    feat: DB関連エラーの基底クラスと派生クラス群を定義

--- src/utils/errors.py
+++ src/utils/errors.py
@@ -2,5 +2,17 @@ class RecordNotFoundError(KeyError):
     """指定 ID のレコードが見つからない場合に送出."""
 
 
-class ConflictError(RuntimeError):
-    """同時更新など競合が起きた場合に送出."""
+class RepositoryError(RuntimeError):
+    """汎用リポジトリのエラー基底クラス."""
+
+
+class NotFoundError(RepositoryError):
+    """指定 ID のレコードが見つからない場合に送出."""
+
+
+class ConflictError(RepositoryError):
+    """競合が発生した場合に送出."""
+
+
+class ValidationError(RepositoryError):  # 使用未定
+    """入力データの検証に失敗した場合に送出."""
