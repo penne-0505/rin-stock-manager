9e61431e08dcda329365372b381e43b8f88757c1
penne <penne0505pp@gmail.com>
2025-05-27 06:19:34 +0900

    fix: FilterOpの変更に伴うimportの修正

--- src/constants/types.py
+++ src/constants/types.py
@@ -1,9 +1,9 @@
 from typing import Any, Mapping, Sequence
 
-from constants.filter_op import Op
+from constants.options import FilterOp
 
 # Filterの型定義 (例：{"status": (Op.EQ, "active"), "id": (Op.IN, [1,2,3])}
-Filter = Mapping[str, tuple[Op, Any | Sequence[Any]]]
+Filter = Mapping[str, tuple[FilterOp, Any | Sequence[Any]]]
 
 # 複合主キーのための、Primary Key (PK) の型定義
 PKMap = Mapping[str, Any]
